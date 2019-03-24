7eee720154b7e16e1372b5b97165b5c3516e89bc
Rumeth <rumeth@users.noreply.github.com>
2019-03-24 18:37:39 +0530

    test: Initial
    Basic Setup Done.

--- src/confirmation/confirmation-dialog.spec.ts
+++ src/confirmation/confirmation-dialog.spec.ts
@@ -1,4 +1,6 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import { NgbxConfirmationConfig } from './confirmation-config';
 
 import {NgbxConfirmationDialog} from './confirmation-dialog';
 
@@ -6,14 +8,19 @@ describe('NgbxConfirmationDialog', () => {
   let component: NgbxConfirmationDialog;
   let fixture: ComponentFixture<NgbxConfirmationDialog>;
 
-  beforeEach(
-      async(() => { TestBed.configureTestingModule({declarations: [NgbxConfirmationDialog]}).compileComponents(); }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({declarations: [NgbxConfirmationDialog], providers: [NgbActiveModal]})
+        .compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(NgbxConfirmationDialog);
     component = fixture.componentInstance;
+    component.ngbxConfirmationOptions = new NgbxConfirmationConfig();
     fixture.detectChanges();
   });
 
-  it('should create', () => { expect(component).toBeTruthy(); });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
